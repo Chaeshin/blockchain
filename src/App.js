048b4deb5ff9f18ee8d99ac19b86a7cbc2eb6d44
fronald143 <120772118+fronald143@users.noreply.github.com>
2024-03-18 00:40:15 +0800

    done partially Add Topic page UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MostCommentedTopics from "./MostCommentedTopics";
 import AddTopicTags from "./AddTopicTags";
-
 
 const App = () => {
   return(
