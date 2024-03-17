ac568db7e1d8cff2f15d4ad1bcf63bd4260fd8a1
fronald143 <120772118+fronald143@users.noreply.github.com>
2024-03-17 22:10:45 +0800

    converted internal css to external

--- src/App.js
+++ src/App.js
@@ -1,111 +1,8 @@
 import { useState } from "react";
+import './App.css';
 
-const styles = {
-  Screen: {
-    backgroundColor: '#ffffff',
-  },
-  Header: {
-    width: '100%',
-    height: '55px',
-    backgroundColor: '#ffffff',
-    boxShadow: '2px -2px 10px rgba(3,3,3,0.1)',
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignContent: 'center',
-  },
-  Icon: {
-    color: '#030303',
-    fill: '#030303',
-    fontSize: '24px',
-    top: '29px',
-    left: '24px',
-    width: '24px',
-    height: '24px',
-    marginLeft: '40px', 
-    marginRight: '20px'
-  },
-  Text: {
-    color: '#030303',
-    fontSize: '20px',
-    fontFamily: 'Roboto',
-    fontWeight: 700,
-    lineHeight: '24px',
-  },
-  MText: {
-    color: '#030303',
-    fontSize: '24px',
-    fontFamily: 'Roboto',
-    fontWeight: 700,
-    lineHeight: '28px',
-    marginTop: '15px',
-    
-  },
-  Card: {
-    width: '920px',
-    height: 'auto',
-    marginRight: '35px', 
-    marginTop: '15px', 
-    marginBottom: '30px',
-    padding: '10px',
-    backgroundColor: '#ffffff',
-    borderRadius: '2px',
-    boxShadow: '2px 0px 10px rgba(3,3,3,0.1)',
-  },
-  PCard: {
-    width: '300px',
-    height: 'auto',
-    paddingBottom: '20px',
-    marginBottom: '20px',
-    backgroundColor: '#ffffff',
-    borderRadius: '2px',
-    boxShadow: '2px 0px 10px rgba(3,3,3,0.1)',
-  },
-  Footer: {
-    width: '100%',
-    height: '55px',
-    display: 'flex',
-    
-    flexWrap: 'wrap',
-    alignContent: 'center',
-    backgroundColor: '#ffffff',
-    boxShadow: '2px -2px 10px rgba(3,3,3,0.1)',
-  },
-  FText: {
-    color: '#030303',
-    fontSize: '16px',
-    fontFamily: 'Roboto',
-    fontWeight: 400,
-    lineHeight: '19px',
-  },
-  LIcon: {
-    color: '#030303',
-    fill: '#030303',
-    fontSize: '40px',
-    width: '40px',
-    height: '40px',
-  },
-};
-
-const IconComponent = () => (
-  <svg style={styles.Icon}  viewBox="0 0 512 512">
-    <path d="M234.5 5.709C248.4 .7377 263.6 .7377 277.5 5.709L469.5 74.28C494.1 83.38 512 107.5 512 134.6V377.4C512 404.5 494.1 428.6 469.5 437.7L277.5 506.3C263.6 511.3 248.4 511.3 234.5 506.3L42.47 437.7C17 428.6 0 404.5 0 377.4V134.6C0 107.5 17 83.38 42.47 74.28L234.5 5.709zM256 65.98L82.34 128L256 190L429.7 128L256 65.98zM288 434.6L448 377.4V189.4L288 246.6V434.6z">
-    </path>
-  </svg>
-);
-const LIconComponent = () => (
-  <svg style={styles.Icon}  viewBox="0 0 24 24">
-    <path fill="none" d="M0 0h24v24H0z">
-    </path>
-    <path d="M18.39 14.56C16.71 13.7 14.53 13 12 13s-4.71.7-6.39 1.56A2.97 2.97 0 0 0 4 17.22V20h16v-2.78c0-1.12-.61-2.15-1.61-2.66zM9.78 12h4.44c1.21 0 2.14-1.06 1.98-2.26l-.32-2.45C15.57 5.39 13.92 4 12 4S8.43 5.39 8.12 7.29L7.8 9.74c-.16 1.2.77 2.26 1.98 2.26z">
-    </path>
-  </svg>
-);
-const defaultProps = {
-  IconComponent,
-  LIconComponent,
-};
-const App = (props) => {
-  const [topics, setTopics] = useState([
+const App = () => {
+  const [topics] = useState([
     { title: 'React', content: 'Can somebody explain me about react?', comments: 1216 },
     { title: 'HTML', content: 'What is HTML?', comments: 1023 },
     { title: 'CSS', content: 'Best practices in styling.', comments: 799 },
@@ -114,43 +11,40 @@ const App = (props) => {
     { title: 'Router', content: 'Best ways to route.', comments: 143 },
   ]);
 
-  const [footer, setfooter] = useState(['Home', 'About Us', 'Contact', 'Term & Condition']);
-  const [nav, setnav] = useState(['Home', 'Top Votes', 'Top Comments']);
-  const [topvote, setTopVote] = useState(['React', 'HTML', 'CSS']);
+  const [footer] = useState(['Home', 'About Us', 'Contact', 'Term & Condition']);
+  const [nav] = useState(['Home', 'Top Votes', 'Top Comments']);
+  const [topvote] = useState(['React', 'HTML', 'CSS']);
 
   return (
-    <div style={styles.Screen}>
-      <div style={styles.Header}>
-        {props.IconComponent
-            ? <props.IconComponent style={styles.Icon} /> 
-            : <defaultProps.IconComponent />}
-        <div style={styles.Text}>
-            blockNote
-        </div>
-        <div style={{width: '80%', display: 'flex', justifyContent: 'flex-end'}}>
+    <div className="screen">
+      <div className="header">
+        <svg className="logoIcon"  viewBox="0 0 512 512">
+          <path d="M234.5 5.709C248.4 .7377 263.6 .7377 277.5 5.709L469.5 74.28C494.1 83.38 512 107.5 512 134.6V377.4C512 404.5 494.1 428.6 469.5 437.7L277.5 506.3C263.6 511.3 248.4 511.3 234.5 506.3L42.47 437.7C17 428.6 0 404.5 0 377.4V134.6C0 107.5 17 83.38 42.47 74.28L234.5 5.709zM256 65.98L82.34 128L256 190L429.7 128L256 65.98zM288 434.6L448 377.4V189.4L288 246.6V434.6z"></path>
+        </svg>
+        <div className="appName">blockNote</div>
+        <div className="navLinks">
           <div style={{width: '30%', display: 'flex', justifyContent: 'space-evenly'}}>
             {nav.map((topic, index) => (
-              <div key={index} style={styles.FText}>{topic}</div>
+              <div key={index} className="topicComments">{topic}</div>
             ))}
           </div>
         </div>
         <div>
-          {props.LIconComponent 
-          ? <props.LIconComponent style={styles.LIcon} /> 
-          : <defaultProps.LIconComponent />}
+          <svg className="logoIcon"  viewBox="0 0 24 24">
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path d="M18.39 14.56C16.71 13.7 14.53 13 12 13s-4.71.7-6.39 1.56A2.97 2.97 0 0 0 4 17.22V20h16v-2.78c0-1.12-.61-2.15-1.61-2.66zM9.78 12h4.44c1.21 0 2.14-1.06 1.98-2.26l-.32-2.45C15.57 5.39 13.92 4 12 4S8.43 5.39 8.12 7.29L7.8 9.74c-.16 1.2.77 2.26 1.98 2.26z"></path>
+          </svg>
         </div>
       </div>
       
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <div style={{display: 'flex', flexDirection:'column'}}>
-        <div style={styles.MText}>
-          Most Commented Topics
-        </div>
-        <div style={styles.Card}>
+        <div className="txtTitle">Most Commented Topics</div>
+        <div className="topicContainer">
           {topics.map((topic, index) => (
-            <div key={index} style={{ width: '100%', height: '85px', display: 'flex',  border: '1px solid #ddd', marginBottom: '2px' }}>
+            <div key={index} className="topicCards">
               <div style={{ width: '80%', paddingLeft: '20px' }}>
-                <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-start', textAlign: 'left' }}>
+                <div className="topicAndDescription">
                   <div style={{ height: '60%' }}><h2>{topic.title}</h2></div>
                   <div style={{ height: '40%' }}>{topic.content}</div>
                 </div>
@@ -162,7 +56,7 @@ const App = (props) => {
       </div>
 
       <div style={{display: 'flex', flexDirection:'column', marginTop:'30px'}}>
-        <div style={styles.PCard}>
+        <div className="popularTopic">
             <div style={{marginTop:'5px', marginLeft:'40px'}}><h3 >Popular Topics</h3></div>
             {topics.map((topic, index) => (
               <div key={index} style={{marginBottom:'3px', marginLeft:'50px', }}> 
@@ -170,7 +64,7 @@ const App = (props) => {
               </div>
             ))}
         </div>
-        <div style={styles.PCard}>
+        <div className="topVoteTopic">
         <div style={{marginTop:'5px', marginLeft:'40px'}}><h3 >Top Vote Topics</h3></div>
             {topvote.map((topic, index) => (
               <div key={index} style={{marginBottom:'3px', marginLeft:'50px', }}> 
@@ -180,10 +74,10 @@ const App = (props) => {
         </div>
       </div>
     </div>
-    <div style={styles.Footer} >
-      <div style={{width: '25%', display: 'flex', justifyContent:'space-around', flexWrap: 'wrap', marginLeft: '30px'}}>
+    <div className="footer">
+      <div className="footerText">
         {footer.map((topic, index) => (
-          <div key={index} style={styles.FText}>{topic}</div>
+          <div key={index}>{topic}</div>
         ))}
       </div>
     </div>
